dd4842cf4524b82ea3678e4d77ede1cdd1210cf8
lalitkumarjangid <lkjlalitkumar2005@gmail.com>
2026-06-12 13:31:10 +0530

    Added vercel.json and the Cors

--- backend/index.js
+++ backend/index.js
@@ -9,8 +9,12 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 app.use(routes);
+app.use(cors({
+  origin: "http://localhost:5173" || "https://simple-api-uxu6.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 mongoose.connect(process.env.MONGODB_URI).then(() => {
   console.log("Connected to MongoDB");
