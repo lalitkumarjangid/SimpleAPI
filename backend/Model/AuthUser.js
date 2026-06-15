3528d4cb1cde97e3744f6cfb9e0cb1d7d00b1bdc
lalitkumarjangid <lkjlalitkumar2005@gmail.com>
2026-06-15 17:52:42 +0530

    fix

--- backend/Model/AuthUser.js
+++ backend/Model/AuthUser.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const authUserSchema = new mongoose.Schema(
   {
     firstName: { type: String, required: true, trim: true },
-    lastName: { type: String, required: true, trim: true },
+    lastName: { type: String, trim: true },
     email: {
       type: String,
       required: true,
